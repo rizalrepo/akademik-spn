20c7b9a990233f82e94c2cdfdf432a26b59be088
rizalrepo <rizalyusei@gmail.com>
2023-07-31 20:29:33 +0800

    fix

--- view/layout/modal.php
+++ view/layout/modal.php
@@ -218,7 +218,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title"><i class="fas fa-file-alt me-2"></i>Laporan Data Jadwal Mengajar</h5>
+                <h5 class="modal-title"><i class="fas fa-file-alt me-2"></i>Laporan Data Kelas Siswa</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
