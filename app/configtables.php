fd88d04da99ae15d58a03b8054b5dc5a195d23fb
rizalrepo <rizalyusei@gmail.com>
2023-08-05 10:51:13 +0800

    .

--- app/configtables.php
+++ app/configtables.php
@@ -5,5 +5,11 @@ $sql_details = array(
     'db'   => 'db_akademik_spn',
     'host' => 'localhost'
 );
+// $sql_details = array(
+//     'user' => 'dpnpious_rizal',
+//     'pass' => 'ao9E21y2Mv',
+//     'db'   => 'dpnpious_akademik_spn',
+//     'host' => 'localhost'
+// );
 
 $con = $sql_details;
